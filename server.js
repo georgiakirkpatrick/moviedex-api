dbb0d8b5874d819b27cfdf82ddcf3326a370d12e
Georgia Kirkpatrick <georgia.kirkpatrick@gmail.com>
2020-06-03 20:10:58 -0700

    Implement authentication

--- server.js
+++ server.js
@@ -11,17 +11,17 @@ const app = express()
 app.use(morgan('dev'))
 app.use(cors())
 
-// app.use(function validateBearerToken(req, res, next) {
-//     const apiToken = process.env.API_TOKEN
-//     const authToken = req.get('Authorization')
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
   
-//     if (!authToken || authToken.split(' ')[1] !== apiToken) {
-//       return res.status(401).json({ error: 'Unauthorized request' })
-//     }
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+      return res.status(401).json({ error: 'Unauthorized request' })
+    }
 
-//     // move to the next middleware
-//     next()
-// })
+    // move to the next middleware
+    next()
+})
 
 app.get('/movies', function handleGetMovies(req, res) {
     let response = MOVIEDATA
@@ -39,7 +39,6 @@ app.get('/movies', function handleGetMovies(req, res) {
     }
 
     if (req.query.avg_vote) {
-        console.log('req.query.avg_vote', req.query.avg_vote)
         response = response.filter(movie =>
             movie.avg_vote >= Number(req.query.avg_vote)
         )
